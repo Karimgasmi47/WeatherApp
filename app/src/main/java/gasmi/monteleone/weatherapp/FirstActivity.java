191ec1c1e4db54e294d9bedbbb24c3f198aca255
Benjamin <benjamin.monteleone@gmail.com>
2017-02-27 19:08:49 +0100

    Recuperation du fichier json en string

--- app/src/main/java/gasmi/monteleone/weatherapp/FirstActivity.java
+++ app/src/main/java/gasmi/monteleone/weatherapp/FirstActivity.java
@@ -1,18 +1,33 @@
 package gasmi.monteleone.weatherapp;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class FirstActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Objets pour récuperer le contenu du fichier json (les villes)
+        String json_file_string = "";
+        InputStream is = null;
+        BufferedReader reader = null;
+        AssetManager assetManager = getAssets();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +41,34 @@ public class FirstActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        try {
+            is = assetManager.open("city_france.json");
+            reader = new BufferedReader(new InputStreamReader(is));
+            json_file_string = reader.readLine();
+            String line = null;
+            while( (line = reader.readLine()) != null) {
+                json_file_string += '\n' + line;
+            }
+            Log.v("FirstActivity", json_file_string);
+        } catch (final Exception e){
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+
+
     }
 
     @Override
