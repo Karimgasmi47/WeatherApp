d65cc686c124b2d4d352ce4331807ee176fc00da
Benjamin <benjamin.monteleone@gmail.com>
2017-03-20 17:42:32 +0100

    CityWeather commentaires

---
+++ app/src/main/java/gasmi/monteleone/weatherapp/CityWeather.java
@@ -0,0 +1,22 @@
+package gasmi.monteleone.weatherapp;
+
+/**
+ * Created by Benjamin on 14/03/2017.
+ */
+
+public class CityWeather {
+
+    // On va mettre autant d'attribut qu'on veut utiliser
+    // et présent dans le fic.json
+
+    // On ira les cherchers avec un deserializer et on init les
+    // attributs de CityWeather grâce à ça
+
+    // Il faudra aller chercher des valeurs en profondeur
+    // càd truc.setMachin( jsonObject.get("tata").getAsJsonObject().get("tutu").getAsString());
+
+
+
+}
+
+
